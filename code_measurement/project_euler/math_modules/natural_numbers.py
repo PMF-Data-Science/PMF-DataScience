ce6f2542bd6289e5dac452fee99f7b5410cb52d2
dorotea17 <58485327+dorotea17@users.noreply.github.com>
2021-11-14 13:31:14 +0100

    Update natural_numbers.py

--- code_measurement/project_euler/math_modules/natural_numbers.py
+++ code_measurement/project_euler/math_modules/natural_numbers.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def get_natural_numbers_sum(lower_limit, divisors, upper_limit):
 
-    '''Summation of numbers in the range of (lower_limit, upper_limit) that are divisible by at least one divisor from the list of divisors'''
+    '''Summation of numbers in the range of <lower_limit, upper_limit> that are divisible by at least one divisor from the list of divisors'''
 
     natural_numbers_generator = (
       number
-      for number in range(lower_limit + 1, upper_limit) '''Summation of numbers in the interval from <lower_limit, upper_limit>'''
+      for number in range(lower_limit + 1, upper_limit) 
       if any(not number % divisor for divisor in divisors)
     )
 
