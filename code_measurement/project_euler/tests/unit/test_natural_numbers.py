969342465fb6939808ff506c026c64a69ebb8a11
dorotea17 <58485327+dorotea17@users.noreply.github.com>
2021-11-14 13:30:41 +0100

    Update test_natural_numbers.py

--- code_measurement/project_euler/tests/unit/test_natural_numbers.py
+++ code_measurement/project_euler/tests/unit/test_natural_numbers.py
@@ -2,7 +2,6 @@ import unittest
 
 from natural_numbers import get_natural_numbers_sum
 
-
 class TestNaturalNumbersSum(unittest.TestCase):
     def test_sum_below_thousand(self, lower_limit, divisors, upper_limit):
         self.assertEqual(get_natural_numbers_sum(lower_limit, divisors, upper_limit), sum(number for number in range(lower_limit+1,upper_limit) if any(not number % divisor for divisor in divisors )))
