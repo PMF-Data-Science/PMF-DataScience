be7e8c701c48f8e49e31024db2da258c70d78c49
dorotea17 <58485327+dorotea17@users.noreply.github.com>
2021-11-14 18:12:41 +0100

    Update test_natural_numbers.py

--- code_measurement/project_euler/tests/unit/test_natural_numbers.py
+++ code_measurement/project_euler/tests/unit/test_natural_numbers.py
@@ -1,13 +1,13 @@
 import unittest
-from random import randint
+from random import randint, sample
 from natural_numbers import get_natural_numbers_sum
 
 class TestNaturalNumbersSum(unittest.TestCase):
     def test_sum_below_thousand(self):
         lower_limit = randint(-1000000, 1000000)
         upper_limit = randint(lower_limit, 1000001)
+        all_divisors = [number for number in range(-20,20) if number != 0]
         divisors_length = randint(1, 10)
-        divisors = []
-        for i in range(divisors_length):
-          divisors.append(randint(-10,10))
-        self.assertEqual(get_natural_numbers_sum(lower_limit, divisors, upper_limit), sum(number for number in range(lower_limit+1,upper_limit) if any(not number % divisor for divisor in divisors )))
+        divisors = sample(all_divisors, divisors_length)
+        expected_result = sum(number for number in range(lower_limit+1,upper_limit) if any(not number % divisor for divisor in divisors ))
+        self.assertEqual(get_natural_numbers_sum(lower_limit, divisors, upper_limit), expected_result)
