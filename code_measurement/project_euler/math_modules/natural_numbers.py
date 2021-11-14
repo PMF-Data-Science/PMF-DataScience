48238f5e70e7850cf94fefca723b474929a62773
dorotea17 <58485327+dorotea17@users.noreply.github.com>
2021-11-14 11:57:41 +0100

    Update natural_numbers.py

--- code_measurement/project_euler/math_modules/natural_numbers.py
+++ code_measurement/project_euler/math_modules/natural_numbers.py
@@ -2,15 +2,12 @@ import numpy as np
 
 def get_natural_numbers_sum(lower_limit, divisors, upper_limit):
 
-    '''Sumiranje brojeva u rasponu od (donja_granica, gornja_granica) koji su djeljivi s barem jednim djeliteljom iz liste divisors'''
-
-    #divisors_set = set(np.random.randint(low=1,high=upper_limit,size=upper_limit - 1))
+    '''Summation of numbers in the range of (lower_limit, upper_limit) that are divisible by at least one divisor from the list of divisors'''
 
     natural_numbers_generator = (
       number
-      for number in range(lower_limit + 1, upper_limit) #budući da se ne uzima upper_limit, ne uzima se ni lower_limit
+      for number in range(lower_limit + 1, upper_limit) '''Summation of numbers in the interval from <lower_limit, upper_limit>'''
       if any(not number % divisor for divisor in divisors)
-      #if sum(map((lambda x: number % x),divisors_set))
     )
 
     return np.sum(np.fromiter(natural_numbers_generator, int))
